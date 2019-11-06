3262dc1ffc3acd19af4bb42336ac2b9291eff074
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-06 23:46:50 +0000

    add helper function to check if tweet text input is invalid and implement in the document ready body

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,60 +4,71 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- // Generate DOM structure for tweet
+// Generate DOM structure for tweet
 
- const createTweetElement = function(tweet) {
-  let $tweet = $(`<article class="tweet">
-  <header>
-    <div class="avatarName">
-      <img class="avatar" src=${tweet.user.avatars} alt="Avatar">
-      <span class="name">${tweet.user.name}</span>
-    </div>
-    <div>
-        <span class="twitter-handle">${tweet.user.handle}</span>
-    </div>
-  </header>
+const createTweetElement = function(tweet) {
+let $tweet = $(`<article class="tweet">
+<header>
+  <div class="avatarName">
+    <img class="avatar" src=${tweet.user.avatars} alt="Avatar">
+    <span class="name">${tweet.user.name}</span>
+  </div>
+  <div>
+      <span class="twitter-handle">${tweet.user.handle}</span>
+  </div>
+</header>
 
 
-    <p  class="tweet-text">${tweet.content.text}</p>
+  <p  class="tweet-text">${tweet.content.text}</p>
 
-    
-  <footer>
-    <div>
-    <span class="date">${getTime(tweet.created_at)} days ago</span>
-    </div>
-    <div class="dateIcons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>  
-    </div>
-  </footer>
-  </article>`);
-  // ...
-  return $tweet;
+  
+<footer>
+  <div>
+  <span class="date">${getTime(tweet.created_at)} days ago</span>
+  </div>
+  <div class="dateIcons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>  
+  </div>
+</footer>
+</article>`);
+// ...
+return $tweet;
 };
 
-  // Helper function to convert time from m/s to day
+// Helper function to convert time from m/s to day
 
-  const getTime = function(created_at) {
-    let oneDay = 1000*60*60*24;
-    let dateCreatedMs = created_at;
-    let todaysDate = Date.now();
-    let differenceMs = todaysDate - dateCreatedMs;
+const getTime = function(created_at) {
+  let oneDay = 1000*60*60*24;
+  let dateCreatedMs = created_at;
+  let todaysDate = Date.now();
+  let differenceMs = todaysDate - dateCreatedMs;
 
-    return Math.round(differenceMs/oneDay);
+  return Math.round(differenceMs/oneDay);
+}
+
+// Helper function that checks if tweet text input matches the invalid cases
+const checkIfTweetIsInvalid = function(tweet) {
+  if (tweet === '') {
+    alert('Empty tweet. Please add text.');
+    return true;
+  } else if ( tweet.length > 140) {
+    alert('You have more than 140 characters.')
+    return true;
   }
+  return false;
+}
 
-  // console.log(getTime(1461116232227));
+// Render tweets 
 
-  // Render tweets 
-
-  const renderTweets = function(tweets) {
-    for (let tweet of tweets) {
-    $('#tweet-container').append(createTweetElement(tweet));
-    }
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+  $('#tweet-container').append(createTweetElement(tweet));
   }
+};
 
+// JQuery 
 $( document ).ready(function() {
   console.log( "ready!" );
 
@@ -67,42 +78,26 @@ $( document ).ready(function() {
     $form.on('submit', function ( event ) {
       console.log('Button clicked, performing ajax call...');
       event.preventDefault();
+      const tweet = $('.tweetInput').val();
+      let result = checkIfTweetIsInvalid(tweet);
+      if (result === false) {
+        $.ajax({
+          url: '/tweets',
+          type: 'POST',
+          data: $(this).serialize()
+        });
+      }
+  });
 
-      $.ajax({
-        url: '/tweets',
-        type: 'POST',
-        data: $(this).serialize()
-      })
-    });
-
-  // Test code
-  const tweetData =  [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" 
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-  renderTweets(tweetData);
+  // Load tweets
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweet) {
+      console.log('Success: ', tweet);
+        renderTweets(tweet);
+    })
+  };
+  loadTweets();
 });
 
 
