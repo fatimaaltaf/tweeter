938bc21bc4dc9aeabb5b6ef247f2d97f0bce3faf
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-06 21:51:20 +0000

    add form submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ // Generate DOM structure for tweet
+
+ const createTweetElement = function(tweet) {
+  let $tweet = $(`<article class="tweet">
+  <header>
+    <div class="avatarName">
+      <img class="avatar" src=${tweet.user.avatars} alt="Avatar">
+      <span class="name">${tweet.user.name}</span>
+    </div>
+    <div>
+        <span class="twitter-handle">${tweet.user.handle}</span>
+    </div>
+  </header>
+
+
+    <p  class="tweet-text">${tweet.content.text}</p>
+
+    
+  <footer>
+    <div>
+    <span class="date">${getTime(tweet.created_at)} days ago</span>
+    </div>
+    <div class="dateIcons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>  
+    </div>
+  </footer>
+  </article>`);
+  // ...
+  return $tweet;
+};
+
+  // Helper function to convert time from m/s to day
+
+  const getTime = function(created_at) {
+    let oneDay = 1000*60*60*24;
+    let dateCreatedMs = created_at;
+    let todaysDate = Date.now();
+    let differenceMs = todaysDate - dateCreatedMs;
+
+    return Math.round(differenceMs/oneDay);
+  }
+
+  // console.log(getTime(1461116232227));
+
+  // Render tweets 
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+    $('#tweet-container').append(createTweetElement(tweet));
+    }
+  }
+
 $( document ).ready(function() {
   console.log( "ready!" );
+
+  // Form submission using JQuery
+    const $form = $('#tweet-submission');
+
+    $form.on('submit', function ( event ) {
+      console.log('Button clicked, performing ajax call...');
+      event.preventDefault();
+
+      $.ajax({
+        url: '/tweets',
+        type: 'POST',
+        data: $(this).serialize()
+      })
+    });
 
   // Test code
   const tweetData =  [
@@ -34,63 +102,10 @@ $( document ).ready(function() {
     }
   ];
 
-
-  // Helper function to convert time from m/s to day
-
-  const getTime = function(created_at) {
-    let oneDay = 1000*60*60*24;
-    let dateCreatedMs = created_at;
-    let todaysDate = Date.now();
-    let differenceMs = todaysDate - dateCreatedMs;
-
-    return Math.round(differenceMs/oneDay);
-  }
-
-  // console.log(getTime(1461116232227));
-
-  // Render tweets 
-
-  const renderTweets = function(tweets) {
-    for (let tweet of tweets) {
-    $('#tweet-container').append(createTweetElement(tweet));
-    }
-  }
-
-  // Generate DOM structure for tweet
-
-  const createTweetElement = function(tweet) {
-    let $tweet = $(`<article class="tweet">
-    <header>
-      <div class="avatarName">
-        <img class="avatar" src=${tweet.user.avatars} alt="Avatar">
-        <span class="name">${tweet.user.name}</span>
-      </div>
-      <div>
-          <span class="twitter-handle">${tweet.user.handle}</span>
-      </div>
-    </header>
-
-
-      <p  class="tweet-text">${tweet.content.text}</p>
-
-      
-    <footer>
-      <div>
-      <span class="date">${getTime(tweet.created_at)} days ago</span>
-      </div>
-      <div class="dateIcons">
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>  
-      </div>
-    </footer>
-    </article>`);
-    // ...
-    return $tweet;
-  };
-
   renderTweets(tweetData);
 });
+
+
 
 
 
